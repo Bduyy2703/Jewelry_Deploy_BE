2d9d0c50c41c44f022adbe61efcf9344a83f8a48
Bduyy2703 <nguyenduy7003@gmail.com>
2024-12-20 20:18:13 +0700

    add Port 8080

--- src/index.js
+++ src/index.js
@@ -131,7 +131,7 @@ app.use((err, req, res, next) => {
 });
 
 // env environments
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
