c7f12b29cf103485ff6bf8a7fd262874bb6a5cec
Bduyy2703 <nguyenduy7003@gmail.com>
2024-12-20 22:47:28 +0700

    test 3

--- src/index.js
+++ src/index.js
@@ -131,7 +131,7 @@ app.use((err, req, res, next) => {
 });
 
 // env environments
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${PORT}`);
 });
